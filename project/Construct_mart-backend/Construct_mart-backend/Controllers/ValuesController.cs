ad468396fb272a4f9b965d2443b8f495083fd877
Sumesh Akalanka <107548452+Sumesh8@users.noreply.github.com>
2023-09-06 16:36:33 +0530

    Add project files

---
+++ project/Construct_mart-backend/Construct_mart-backend/Controllers/ValuesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Construct_mart_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult UsersGet()
+        {
+            var users = new string[] { "sumesh", "akalanka", "fuck" };
+            return Ok(users);
+        }
+
+        [HttpPost]
+        public IActionResult UsersPost()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UsersUpdate()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult UsersDelete()
+        {
+            var somthingWentWrong = true;
+            if (somthingWentWrong) return BadRequest();
+            return Ok();
+        }
+    }
+}
